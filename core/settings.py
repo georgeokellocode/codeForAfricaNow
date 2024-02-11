5d2eda08b2c69d7026f9a5f4f09bdc000406d324
georgeokellocode <159538874+georgeokellocode@users.noreply.github.com>
2024-02-11 18:51:00 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-ej16)up%h=2%^=hn90zv+m%%i&-dh%wgzl77x=sd$)l3+pyy%5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://codeforafricanow.onrender.com']
+ALLOWED_HOSTS = ['codeforafricanow.onrender.com']
 
 
 # Application definition
